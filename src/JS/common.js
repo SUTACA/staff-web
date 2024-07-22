9c9e6fd3af7af9fc96265bd3e0019f2848c1df60
なぎと-nagito- <hello@nagito.work>
2024-07-22 19:33:13 +0900

    Camera読み取り機能

---
+++ src/JS/common.js
@@ -0,0 +1,14 @@
+fetch("/header.html")
+.then((response) => response.text())
+.then((data) => document.querySelector("#header").innerHTML = data);
+function toggleMenu() {
+var menuItems = document.getElementById('menuItems');
+menuItems.classList.toggle('open');
+}
+// メニューを閉じる
+window.addEventListener('click', function (e) {
+var menuItems = document.getElementById('menuItems');
+if (!e.target.closest('.menu-bar')&&!e.target.closest('.menu-items') && menuItems.classList.contains('open')) {
+    menuItems.classList.remove('open');
+}
+});
