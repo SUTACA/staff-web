653f1b342612d37877563d0a13cf08f4b6543f46
なぎと-nagito- <hello@nagito.work>
2024-08-22 21:57:04 +0900

    修正

--- src/JS/common.js
+++ src/JS/common.js
@@ -40,33 +40,37 @@ var sessionData = {
     "data": {},//データ
     "locationId": "",//スキャンポイント
 };
-//login.html以外だったら実行
+
+// login.html以外だったら実行
 if (!location.pathname.endsWith('/login.html')) {
-    //セッションデータの取得
+    // セッションデータの取得
     if (sessionStorage.getItem('sessionData')) {
         let sessionData = JSON.parse(sessionStorage.getItem('sessionData'));
-        //セッションの有効期限が切れている場合
+        // セッションの有効期限が切れている場合
         if (sessionData.sessionExpire <= new Date().getTime()) {
             sessionStorage.removeItem('sessionData');
-            location.href = './login.html?error=2';
+            // 無限ループを防ぐためにリダイレクトする前にフラグを設定
+            if (!location.search.includes('error=2')) {
+                location.href = './login.html?error=2';
+            }
         } else {
-            //セッションの有効期限を更新
+            // セッションの有効期限を更新
             sessionData.sessionExpire = new Date().getTime() + JSON.parse(sessionStorage.getItem('config')).loginSessionTime * 60000;
             sessionData.sessionExpire2 = new Date(sessionData.sessionExpire).toLocaleString();
             sessionStorage.setItem('sessionData', JSON.stringify(sessionData));
         }
-        //メインコンテンツの表示
+        // メインコンテンツの表示
         const validPages = ['/index.html', '/dino.html', '/qanda.html', '/lost.html'];
         if (!validPages.some(page => location.pathname.endsWith(page))) {
             location.href = './index.html';
         }
     } else {
-        //ログインページにリダイレクト
-        location.href = './login.html?error=3';
+        // ログインページにリダイレクト
+        if (!location.search.includes('error=3')) {
+            location.href = './login.html?error=3';
+        }
     }
 }
-
-
 
 
 
